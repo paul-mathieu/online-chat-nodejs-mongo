6e4e927453532ce92a4ff8ea847477b9b44003e3
Paul Mathieu <>
2021-04-05 12:38:44 +0200

    upgrade version syntax

--- public/client.js
+++ public/client.js
@@ -1,136 +1,93 @@
 /*global io*/
 /*jslint browser: true*/
-var socket = io();
-var i;
+let socket = io()
+let i
 
-var userLogged;
-var userSelected;
+let userLogged
+let userSelected
 
-/*** Fonctions utiles ***/
+/*** Utils ***/
 
-/**
- * Scroll vers le bas de page si l'utilisateur n'est pas remonté pour lire d'anciens messages
- */
+// Scroll vers le bas de page si l'utilisateur n'est pas remonté pour lire d'anciens messages
 function scrollToBottom() {
   if ($(window).scrollTop() + $(window).height() + 2 * $('#messages li').last().outerHeight() >= $(document).height()) {
-    $('html, body').animate({ scrollTop: $(document).height() }, 0);
+    $('html, body').animate({ scrollTop: $(document).height() }, 0)
   }
 }
 
-/*** Gestion des événements ***/
+/*** Events ***/
 
-/**
- * Connexion de l'utilisateur
- * Uniquement si le username n'est pas vide et n'existe pas encore
- */
-$('#login form').submit(function (e) {
-  e.preventDefault();
-  var user = {
-    username: $('#login input').val().trim()
-  };
-  if (user.username.length > 0) { // Si le champ de connexion n'est pas vide
-    // on inscrit l'utilisateur à la liste si n'est pas inscrit
-    login(socket, user);
-    // signUpUser(socket, user);
-    // signInUser(socket, user);
-  }
-});
-
-/**
- * Envoi d'un message
- */
-$('#chat form').submit(function (e) {
-  e.preventDefault();
-  var message = {
-    text: $('#m').val(),
-    to: userSelected
-  };
-  $('#m').val('');
-  if (message.text.trim().length !== 0 && message.to != null) { // Gestion message vide
-    socket.emit('chat-message', message);
-  }
-  $('#chat input').focus(); // Focus sur le champ du message
-});
-
-/**
- * Réception d'un message
- */
-socket.on('chat-message', function (message) {
-  if (message.from == userSelected || message.to == userSelected) {
-    $('#messages').append($('<li class="message">').html('<span class="username">' + message.from + '</span> ' + message.text));
-  }
-  scrollToBottom();
-});
-
-/**
- * Réception d'un message de service
- */
-socket.on('service-message', function (message) {
-  $('#messages').append($('<li class="' + message.type + '">').html('<span class="info">information</span> ' + message.text));
-  scrollToBottom();
-});
-
-socket.on('load-user', function (user) {
-  $('#users').append($('<li id="' + user + '" class="loggedOut">' + user + '</li>'))
-  $('#' + user).click(function () {
-    if (userLogged != user) {
-      userSelected = user;
-      $(".active").removeClass("active");
-      $(this).addClass("active");
-      var selector = 'li.message'; // enlève les messages présents
-      $(selector).remove();
-      socket.emit('load-previous-messages', userLogged, userSelected);
-    }
-  })
-});
-
-/**
- * Connexion d'un nouvel utilisateur
- */
-socket.on('user-is-logged-in', function (user) {
-  console.log("user-is-logged-in");
-  $('#' + user).removeClass("loggedOut");
-  console.log("remove class");
-  $('#' + user).addClass('new');
-  setTimeout(function () {
-    $('#users li.new').removeClass('new');
-  }, 1000);
-});
-
-/**
- * Déconnexion d'un utilisateur
- */
-socket.on('user-is-logged-out', function (user) {
-  var selector = '#' + user;
-  $(selector).addClass('loggedOut');
-});
-
-socket.on('remove-current-users-list', function () {
-  const selector = '#users li';
-  $(selector).remove();
-  console.log("remove-current-users-list");
+// Connexion de l'utilisateur
+// Uniquement si le username n'est pas vide et n'existe pas encore
+$('#login form').submit((e) => {
+  e.preventDefault()
+  let user = {username: $('#login input').val().trim()}
+  // Si le champ de connexion n'est pas vide on inscrit l'utilisateur à la liste si n'est pas inscrit
+  if (user.username.length > 0) login(socket, user)
 })
 
+// Envoi d'un message
+$('#chat form').submit((e) => {
+  e.preventDefault()
+  let message = {text: $('#m').val(), to: userSelected}
+  $('#m').val('')
+  // Gestion message vide
+  if (message.text.trim().length !== 0 && message.to != null) socket.emit('chat-message', message)
+  // Focus sur le champ du message  
+  $('#chat input').focus() 
+})
 
+// Réception d'un message
+socket.on('chat-message', (message) => {
+  if (message.from == userSelected || message.to == userSelected) {
+    $('#messages').append($('<li class="message">').html('<span class="username">' + message.from + '</span> ' + message.text))
+  }
+  scrollToBottom()
+})
 
+// Réception d'un message de service
+socket.on('service-message', (message) => {
+  $('#messages').append($('<li class="' + message.type + '">').html('<span class="info">information</span> ' + message.text))
+  scrollToBottom()
+})
 
-function signUpUser(socket, user, callback) {
-  socket.emit('user-signup', user, callback);
-}
-
-function signInUser(socket, user) {
-  socket.emit('user-login', user, function (success) {
-    if (success) {
-      userLogged = user.username;
-      $('body').removeAttr('id'); // Cache formulaire de connexion
-      $('#chat input').focus(); // Focus sur le champ du message
+socket.on('load-user', (user) => {
+  $('#users').append($('<li id="' + user + '" class="loggedOut">' + user + '</li>'))
+  $('#' + user).click(() => {
+    if (userLogged != user) {
+      userSelected = user
+      $(".active").removeClass("active")
+      $(this).addClass("active")
+      let selector = 'li.message' // enlève les messages présents
+      $(selector).remove()
+      socket.emit('load-previous-messages', userLogged, userSelected)
     }
-  });
+  })
+})
+
+// Connexion d'un nouvel utilisateur
+socket.on('user-is-logged-in', (user) => {
+  $('#' + user).removeClass("loggedOut")
+  $('#' + user).addClass('new')
+  setTimeout(() => $('#users li.new').removeClass('new'), 1000)
+});
+
+// Déconnexion d'un utilisateur
+socket.on('user-is-logged-out', (user) => $('#' + user).addClass('loggedOut'))
+socket.on('remove-current-users-list', () => $('#users li').remove())
+
+const signUpUser = (socket, user, callback) => socket.emit('user-signup', user, callback)
+
+const signInUser = (socket, user) => {
+  socket.emit('user-login', user, (success) => {
+    if (success) {
+      userLogged = user.username
+      $('body').removeAttr('id') // Cache formulaire de connexion
+      $('#chat input').focus() // Focus sur le champ du message
+    }
+  })
 }
 
 const login = async (socket, user) => {
-  const result = await signUpUser(socket, user, function (res) {
-    console.log(res, "login")
-    signInUser(socket, user);
-  });
+  const result = await signUpUser(socket, user, (res) => signInUser(socket, user))
 }
--- server.js
+++ server.js
@@ -1,150 +1,106 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// connection with socket
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
-//Use chalk to add colours on the console
-const chalk = require('chalk');
+// Add colours on the console
+require('colors')
 
-
-const redisQuery = require('./redisQuery/index.js');
-const mongoQuery = require('./mongoQuery/index.js');
-const mongoRedisQuery = require('./mongoRedisQuery.js');
+const redisQuery = require('./lib/redisQuery.js')
+const mongoQuery = require('./lib/mongoQuery.js')
+const mongoRedisQuery = require('./lib/mongoRedisQuery.js')
 
 /**
  * Management of users' HTTP requests by returning files from the 'public' folder
  */
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'))
 
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
 
-  /**
-   * User connected to the socket
-   */
-  var loggedUser;
+  // User connected to the socket
+  let loggedUser
 
+  // User registration in mongo
+  socket.on('user-signup', (user, callback) => mongoQuery.insertNewUserToMongo(user, (res) => callback(res)))
 
-  /**
-   * User registration in mongo
-   */
-  socket.on('user-signup', function (user, callback) {
-    mongoQuery.insertNewUserToMongo(user, function (res) {
-      callback(res);
-    })
-  });
-
-  /**
-  * Connection of a user via the form:
-  */
-  socket.on('user-login', function (user, callback) {
-
-
-    // Checking that the user is not already logged in
-
-    let isUserLoggedIn;
-
-    redisQuery.userAlreadyLoggedIn(user.username, function (isAlreadyLoggedIn) {
-      isUserLoggedIn = isAlreadyLoggedIn;
-    });
-
-    console.log("isUserLoggedIn", isUserLoggedIn);
-
-    mongoQuery.isUserRegistered(user, function (userRegistered) {
-      if (user.username !== undefined && !isUserLoggedIn && userRegistered) { // S'il est bien nouveau
-
+  // Connection of a user via the form:
+  socket.on('user-login', (user, callback) => {
+    mongoQuery.isUserRegistered(user, (userRegistered) => {
+      // Checking that the user is not already logged in
+      if (user.username !== undefined && !redisQuery.userAlreadyLoggedIn(user.username) && userRegistered) { // S'il est bien nouveau
         // Save the user and add to the connected list
-        loggedUser = user.username;
-
+        loggedUser = user.username
         // Saving the logged in user in redis
-        redisQuery.storeUserConnectedToRedis(loggedUser);
-
-        mongoRedisQuery.refreshUsersList(io);
-
-        socket.join(loggedUser);
-
+        redisQuery.storeUserConnectedToRedis(loggedUser)
+        mongoRedisQuery.refreshUsersList(io)
+        socket.join(loggedUser)
         // Sending service messages
-        sendServiceMessage(socket, loggedUser, 'login');
-        sendServiceMessage(socket, loggedUser, 'login', broadcast = true);
-
+        sendServiceMessage(socket, loggedUser, 'login')
+        sendServiceMessage(socket, loggedUser, 'login', broadcast = true)
         // Issue of 'user-login' and callback call
-        io.emit('user-login', loggedUser);
-        callback(true);
-      } else {
-        callback(false);
-      }
+        io.emit('user-login', loggedUser)
+        console.log(('User ' + loggedUser + ' connected').magenta)
+        // callback
+        callback(true)
+      } else callback(false)
     })
-  });
+  })
 
-  /**
-   * Logging out of a user
-   */
-  socket.on('disconnect', function () {
+  // Logging out of a user
+  socket.on('disconnect', () => {
     if (loggedUser !== undefined) {
       // Broadcast of a 'service-message'
-      sendServiceMessage(socket, loggedUser, 'logout', broadcast = true);
-
+      sendServiceMessage(socket, loggedUser, 'logout', broadcast = true)
       // Removing the user from Redis
-      redisQuery.removeUserConnectedFromRedis(loggedUser);
-
+      redisQuery.removeUserConnectedFromRedis(loggedUser)
       // Issue of a 'user-logout' containing the user
-      io.emit('user-is-logged-out', loggedUser);
+      io.emit('user-is-logged-out', loggedUser)
+      // Log info
+      console.log(('User ' + loggedUser + ' disconnected').magenta)
     }
-  });
+  })
 
-  /**
-   * Reception of the 'chat-message' event and re-transmission to all users
-   */
-  socket.on('chat-message', function (message) {
+  // Reception of the 'chat-message' event and re-transmission to all users
+  socket.on('chat-message', (message) => {
+    // Add the username to the message and we send the event
+    message.from = loggedUser
+    mongoQuery.storeMsgToMongo(message)
+    io.to(message.to).to(message.from).emit('chat-message', message)
+  })
 
-    //We add the username to the message and we send the event
-    message.from = loggedUser;
-
-    mongoQuery.storeMsgToMongo(message);
-
-    io.to(message.to).to(message.from).emit('chat-message', message);
-  });
-
-  /**
-   * load previous messages of user
-   */
-  socket.on('load-previous-messages', function (user1, user2) {
-    mongoQuery.loadMsgFromMongo(user1, user2, socket);
-  });
+  // load previous messages of user
+  socket.on('load-previous-messages', (user1, user2) => {
+    mongoQuery.loadMsgFromMongo(user1, user2, socket)
+  })
 });
 
 /**
  * Starting the server by listening for connections arriving on port 3000
  */
-http.listen(3000, function () {
-  console.log('Server is listening on *:3000');
-});
+http.listen(3000, () => console.log('Server is listening on *:3000'.bold.green))
 
-
-
-function sendServiceMessage(socket, loggedUser, type, broadcast = false) {
+const sendServiceMessage = (socket, loggedUser, type, broadcast = false) => {
   if (broadcast) {
-    if (type == 'login') {
-      var broadcastedServiceMessage = {
+    if (type == 'login') { // if login
+      let broadcastedServiceMessage = {
         text: 'User "' + loggedUser + '" logged in',
         type: type
-      };
-      socket.broadcast.emit('service-message', broadcastedServiceMessage);
-    }
-    if (type == 'logout') {
-      var serviceMessage = {
+      }
+      socket.broadcast.emit('service-message', broadcastedServiceMessage)
+    } else if (type == 'logout') { // if logout
+      let serviceMessage = {
         text: 'User "' + loggedUser + '" is logged out',
         type: type
-      };
-      socket.broadcast.emit('service-message', serviceMessage);
+      }
+      socket.broadcast.emit('service-message', serviceMessage)
     }
-
-  }
-  else {
+  } else { // if current login
     var userServiceMessage = {
       text: 'You logged in as "' + loggedUser + '"',
       type: type
-    };
-    socket.emit('service-message', userServiceMessage);
+    }
+    socket.emit('service-message', userServiceMessage)
   }
 }
