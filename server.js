3f3e2b5f632b1b9e3d5b542efd468e6f164fd374
Paul Mathieu <>
2021-04-07 17:19:45 +0200

    add connection info

--- server.js
+++ server.js
@@ -7,19 +7,18 @@ const io = require('socket.io')(http)
 // Add colours on the console
 require('colors')
 
+const core = require('./lib/core.js')
 const redisQuery = require('./lib/redis.js')
 const mongoQuery = require('./lib/mongo.js')
 const mongoRedisQuery = require('./lib/mongo-redis.js')
 
-/**
- * Management of users' HTTP requests by returning files from the 'public' folder
- */
+// Management of users' HTTP requests by returning files from the 'public' folder
 app.use('/', express.static(__dirname + '/public'))
 
 
 io.on('connection', (socket) => {
 
-  // User connected to the socket
+  // User connected
   let loggedUser
 
   // User registration in mongo
@@ -35,10 +34,11 @@ io.on('connection', (socket) => {
         // Saving the logged in user in redis
         redisQuery.storeUserConnectedToRedis(loggedUser)
         mongoRedisQuery.refreshUsersList(io)
+        // mongoQuery.addConnection()
         socket.join(loggedUser)
         // Sending service messages
-        sendServiceMessage(socket, loggedUser, 'login')
-        sendServiceMessage(socket, loggedUser, 'login', broadcast = true)
+        core.sendServiceMessage(socket, loggedUser, 'login') // to socket
+        core.sendServiceMessage(socket, loggedUser, 'login', broadcast = true) // to others
         // Issue of 'user-login' and callback call
         io.emit('user-login', loggedUser)
         console.log(('User ' + loggedUser + ' connected').magenta)
@@ -48,11 +48,14 @@ io.on('connection', (socket) => {
     })
   })
 
+  // Reception d'un log de connection
+  socket.on('info-connection', (infoConnection) => mongoQuery.addInfoConnection(infoConnection))
+
   // Logging out of a user
   socket.on('disconnect', () => {
     if (loggedUser !== undefined) {
       // Broadcast of a 'service-message'
-      sendServiceMessage(socket, loggedUser, 'logout', broadcast = true)
+      core.sendServiceMessage(socket, loggedUser, 'logout', broadcast = true)
       // Removing the user from Redis
       redisQuery.removeUserConnectedFromRedis(loggedUser)
       // Issue of a 'user-logout' containing the user
@@ -66,41 +69,14 @@ io.on('connection', (socket) => {
   socket.on('chat-message', (message) => {
     // Add the username to the message and we send the event
     message.from = loggedUser
+    message.dateServer = new Date().toISOString()
     mongoQuery.storeMsgToMongo(message)
     io.to(message.to).to(message.from).emit('chat-message', message)
   })
 
   // load previous messages of user
-  socket.on('load-previous-messages', (user1, user2) => {
-    mongoQuery.loadMsgFromMongo(user1, user2, socket)
-  })
-});
+  socket.on('load-previous-messages', (userTo, userFrom) => mongoQuery.loadMsgFromMongo(userTo, userFrom, socket))
+})
 
-/**
- * Starting the server by listening for connections arriving on port 3000
- */
+// Starting the server by listening for connections arriving on port 3000
 http.listen(3000, () => console.log('Server is listening on *:3000'.bold.green))
-
-const sendServiceMessage = (socket, loggedUser, type, broadcast = false) => {
-  if (broadcast) {
-    if (type == 'login') { // if login
-      let broadcastedServiceMessage = {
-        text: 'User "' + loggedUser + '" logged in',
-        type: type
-      }
-      socket.broadcast.emit('service-message', broadcastedServiceMessage)
-    } else if (type == 'logout') { // if logout
-      let serviceMessage = {
-        text: 'User "' + loggedUser + '" is logged out',
-        type: type
-      }
-      socket.broadcast.emit('service-message', serviceMessage)
-    }
-  } else { // if current login
-    var userServiceMessage = {
-      text: 'You logged in as "' + loggedUser + '"',
-      type: type
-    }
-    socket.emit('service-message', userServiceMessage)
-  }
-}
